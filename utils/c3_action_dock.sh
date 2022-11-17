54f68e8139d9ee4b20495c3f66433273e146505c
slowrunner <slowrunner@users.noreply.github.com>
2022-11-17 12:19:41 -0500

    initial

---
+++ utils/c3_action_dock.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo -e "\n*** Send Create3 Dock Action"
+echo '*** ros2 action send_goal /undock irobot_create_msgs/action/DockServo "{}"'
+ros2 action send_goal /dock irobot_create_msgs/action/DockServo "{}"
+
+# Humble
+# echo '*** ros2 action send_goal /undock irobot_create_msgs/action/Dock "{}"'
+# ros2 action send_goal /dock irobot_create_msgs/action/Dock "{}"
