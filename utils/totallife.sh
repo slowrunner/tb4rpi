9ff865671d4ffedfcc71570a5b070adc350f7aa9
slowrunner <slowrunner@users.noreply.github.com>
2022-11-18 03:58:50 +0000

    initial

---
+++ utils/totallife.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+# totallife.sh    print total hours and sessions of life in life.log
+#
+# requires bc  (sudo apt-get install bc)
+#
+echo "TOTAL Turtlebot 4 Lite LIFE STATISTICS"
+echo "(Cleaning life.log first)"
+/home/ubuntu/tb4rpi/plib/cleanlifelog.py
+echo " "
+fn="/home/ubuntu/tb4rpi/life.log"
+totalLife=`(awk -F':' '{sum+=$3}END{print sum;}' $fn)`
+echo "Total Life: " $totalLife "hrs (since Nov 17, 2022)"
+echo "Sessions (boot): " `(grep -c "\- boot \-" $fn)`
+booted=`(grep -c "\- boot \-" $fn)`
+aveSession=`(echo "scale=1; ($totalLife / $booted)" | bc -l)`
+echo "Average Session: " $aveSession "hrs"
