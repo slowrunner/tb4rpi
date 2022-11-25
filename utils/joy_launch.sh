49ece7fea78495170d4a1478bd3afb7c71417d36
slowrunner <slowrunner@users.noreply.github.com>
2022-11-25 04:40:54 +0000

    SNES Gamepad configured

---
+++ utils/joy_launch.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# FILE: joy_launch.sh
+
+echo -e "\n*** Launching Joy Teleop"
+echo "ros2 launch turtlebot4_bringup joy_teleop.launch.py"
+ros2 launch turtlebot4_bringup joy_teleop.launch.py 
